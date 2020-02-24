1df57d119445ff6ddc0569d27ef9b7c895636e71
Jan Mrkos <mrkosjan@gmail.com>
2020-02-24 17:54:26 +0100

    pricing problem start

--- src/PricingProblem.jl
+++ src/PricingProblem.jl
@@ -1,12 +1,3 @@
 module PricingProblem
-export State
-
-
-CT = Int64
-
-# C   :: Vector{CT}
-# C_init  :: Vector{CT}
-
-println("Done")
 
 end
--- test/test_pricing_problem.jl
+++ test/test_pricing_problem.jl
@@ -1,2 +1,17 @@
 using Mdp.PricingProblem
-# S = State([1,2,3])
+
+capacity_ranges = (0:2, 0:2, 0:2) # Capacity of problem capacity edges
+selling_period_ends = (2,3,4) # timestep after which the corresponding capacity edge can not be sold
+N = 4  # Number of timesteps
+S = Base.product(capacity_ranges...) # States defined by available capacity
+A = [10,20,30,1000] # price range <-> actions
+
+function following_states(s, a, s_next, n) # Transition probability and reward for transitions between states
+
+
+end
+
+
+for v in S
+    println(v)
+end
