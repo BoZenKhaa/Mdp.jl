f1bae012fc179673aaf8c7c3571c7cc1e9d36eee
Jan Mrkos <mrkosjan@gmail.com>
2020-02-25 21:41:10 +0100

    wip problem

--- src/PricingProblem.jl
+++ src/PricingProblem.jl
@@ -1,3 +1,11 @@
 module PricingProblem
 
+const selling_period_ends = (3,5,3)
+
+req = [0,1]
+λ = [2,2,2] # [[2,2],[2]]
+N = 5
+
+function prob_edge_req(req, ts)
+    return λ[req_ind(req)]/selling_period_end(req)
 end
