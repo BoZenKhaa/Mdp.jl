841c8254531c7ce44257b6d3a0425dc61c9ce62b
Jan Mrkos <mrkosjan@gmail.com>
2020-02-26 14:43:25 +0100

    Problem definition struct

--- src/PricingProblem.jl
+++ src/PricingProblem.jl
@@ -1,11 +1,50 @@
 module PricingProblem
 
-const selling_period_ends = (3,5,3)
+export Product, Problem, prob_user_accept, prob_prod_req, len_product_selling_period
 
-req = [0,1]
-λ = [2,2,2] # [[2,2],[2]]
-N = 5
+Product = NTuple{N, Int64} where N
 
-function prob_edge_req(req, ts)
-    return λ[req_ind(req)]/selling_period_end(req)
+struct Problem
+    capacity_ranges::NTuple{N, AbstractArray} where N # Capacity of problem capacity edges
+    edge_selling_horizon_end::NTuple{N, Int64} where N # timestep after which the corresponding capacity edge can not be sold
+    N::Int64  # Number of timesteps
+    A::NTuple{N, Int64} where N # price range <-> actions
+    products::NTuple{N, Product} where N
+    λ::Dict{Product, Int64}
+
+    S::Base.Iterators.ProductIterator # States defined by available capacity
+
+    Problem(capacity_ranges, edge_selling_horizon_end, N, A, products, λ)=
+        new(capacity_ranges, edge_selling_horizon_end, N, A, products, λ,
+            Base.product(capacity_ranges...)
+            )
+end
+
+function len_product_selling_period(product::Product, pr::Problem)
+    pr.edge_selling_horizon_end[product[1]]
+end
+
+function prob_user_accept(
+                        x::Number;
+                        slope_start::Float64=5.,
+                        slope_end::Float64=10.)
+    # "Step" function with slope in the middle, like so:
+    #  1-|-----\
+    #    |      \
+    #    |       \
+    #  0-|-----|--\------
+    #    0  start end
+    if x<slope_start
+        return 1.
+    elseif x>slope_end
+        return 0.
+    else
+        return (x-slope_end)/(slope_start-slope_end)
+    end
+end
+
+function prob_prod_req(product::Product, timestep::Int64, pr::Problem)
+    return pr.λ[product]/len_product_selling_period(product, pr)
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,54 @@ using Mdp
 using Test
 
 @testset "VI.jl" begin
-    # Write your own tests here.
     include("test_vi.jl")
 end
 
 @testset "FH.jl" begin
-    # Write your own tests here.
     include("test_fh.jl")
 end
 
 @testset "PricingProblem.jl" begin
-    # Write your own tests here.
-    include("test_pricing_problem.jl")
+    # include("test_pricing_problem.jl")
+
+using Mdp.PricingProblem
+
+# Model of problem:
+"""
+Model of problem:
+
+3 nodes, 2 edges:
+graph:      A---B---C
+capacity:     1   1
+sell end:     3   5
+"""
+
+P = Problem(
+    (0:1, 0:1),             # Capacity of edges
+    (3, 5),                 # Selling period end of edges
+    5,                      # Number of timesteps (Start at 1)
+    (10, 20, 1000),         # Actions (prices)
+    ((1,), (2,), (1, 2)),   # Products (seqeuences of edge indeces)
+    Dict((1,) => 2,         # λ: Dictionary of demand intensities for products
+        (2,) => 2,
+        (1, 2) => 2),
+)
+
+
+@testset "acc_prob" begin
+    @test prob_user_accept(5.0; slope_start = 5.0, slope_end = 10.0) == 1.0
+    @test prob_user_accept(10.0; slope_start = 5.0, slope_end = 10.0) == 0.0
+    @test prob_user_accept(7.5; slope_start = 5.0, slope_end = 10.0) == 0.5
+    @test prob_user_accept(3.0; slope_start = 5.0, slope_end = 10.0) == 1.0
+    @test prob_user_accept(15.0; slope_start = 5.0, slope_end = 10.0) == 0.0
+end
+
+@testset "req_prob" begin
+    @test len_product_selling_period((1,), P) == P.edge_selling_horizon_end[1]
+    @test len_product_selling_period((2,), P) == P.edge_selling_horizon_end[2]
+    @test len_product_selling_period((1, 2), P) == P.edge_selling_horizon_end[1]
+
+    @test prob_prod_req((1,), 1, P) == P.λ[(1,)] / P.edge_selling_horizon_end[1]
+    @test prob_prod_req((1, 2), 1, P) == P.λ[(1, 2)] / P.edge_selling_horizon_end[1]
+end
 end
